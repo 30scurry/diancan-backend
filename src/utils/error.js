50424b43af938425c3427a20161c86dce98548cc
30scurry <53144703+30scurry@users.noreply.github.com>
2025-02-18 20:49:43 +0800

    Create error.js

---
+++ src/utils/error.js
@@ -0,0 +1,30 @@
+class AppError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+const handleError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    console.error('错误 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: '服务器内部错误'
+    });
+  }
+};
+
+module.exports = {
+  AppError,
+  handleError
+};
